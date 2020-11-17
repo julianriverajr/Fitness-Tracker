2ac880733111d900f5d18bed465f16aca059e16c
Julian Rivera Jr <julianriverajr@gmail.com>
2020-11-16 20:39:57 -0600

    tried to finish server js

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true})
