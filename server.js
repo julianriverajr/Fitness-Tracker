6d115dd296a4b68181319199ff9586e8e01ec773
Julian Rivera Jr <julianriverajr@gmail.com>
2020-11-23 21:39:34 -0600

    finished server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(morgan("dev"));
 
@@ -11,4 +11,17 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true})
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
+    useUnifiedTopology:true, useNewUrlParser:true
+})
+.then(function(){
+    console.log("connected to mongodb")
+})
+.catch(function(err){
+    console.error("could not connect to mongodb", err)
+})
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+app.listen(PORT, function(){
+    console.log(`App running on port ${PORT}`);
+})
