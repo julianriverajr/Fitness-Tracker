9627051e297e9649f61aab2565a180ad09871b71
Julian Rivera Jr <julianriverajr@gmail.com>
2020-11-23 21:40:45 -0600

    attempted to fix seed.js file as it wouldnt seed

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
-let db = require("../models");
+let db = require("../models/workout");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect("mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
