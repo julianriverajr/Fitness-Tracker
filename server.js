8122c2e0eba20ce68846a60dbb4765b1c08b5556
Julian Rivera Jr <julianriverajr@gmail.com>
2020-11-28 18:07:12 -0600

    deploying

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ app.use(express.json());
 app.use(express.static('public'));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
-    useUnifiedTopology:true, useNewUrlParser:true
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true,
+    useFindAndModify:false
 })
 .then(function(){
     console.log("connected to mongodb")
